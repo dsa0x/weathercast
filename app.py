f8b8db97d8aa0b197c8233ceabcbcb9882924154
Dare Samsondeen <dsamsondeen@gmail.com>
2019-08-29 11:44:11 +0200

    newest

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import config
 import requests
 from urllib.parse import urlencode, quote_plus
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "os.environ.get('SECRET')"
@@ -25,13 +26,18 @@ def weather(lat, lon):
         result)
     r = requests.get(weather_url)
     weather = r.json()
+    temperature = int(weather['main']['temp'])
+    description = weather['weather'][0]['description']
+    main = weather['weather'][0]['main']
+    icon = weather['weather'][0]['icon']
+    ip = get_ip()
+    city = ip['city']
+    country = ip['country']
+    lists = getweather3hr(lat, lon)
     if weather['cod'] == 200:
-        return {
-            'Temperature': weather['main']['temp'],
-            'Weather Description': weather['weather'][0]['description']
-        }
+        return render_template('index.html', temperature=temperature, description=description, icon=icon, city=city, country=country,main=main, lists=lists)
     else:
-        return None
+        return redirect(url_for('error_404'))
 
 def get_ip():
     #Get Ip address from user header
@@ -43,7 +49,6 @@ def get_ip():
     r = requests.get('http://ip-api.com/json/{}'.format(ip))
     ip_api = r.json()
     if ip_api['status'] == 'success':
-        print(ip_api)
         return {
             'status': ip_api['status'],
             'city': ip_api['city'],
@@ -58,10 +63,40 @@ def get_ip():
         return None
 
 
+def getweather3hr(lat,lon):
+    params = {'lat': lat, 'lon': lon,
+              'appid': config.WEATHER_KEY, 'units': 'metric'}
+    result = urlencode(params, quote_via=quote_plus)
+    weather_url = 'https://api.openweathermap.org/data/2.5/forecast?{}'.format(
+        result)
+    r = requests.get(weather_url)
+    weather = r.json()
+    listemp = []
+    listdesc = []
+    listmain = []
+    listicon = []
+    listime = []
+    for i in range(1,7):
+        listemp.append(int(weather['list'][i]['main']['temp']))
+        listdesc.append(weather['list'][i]['weather'][0]['description'])
+        listmain.append(weather['list'][i]['weather'][0]['main'])
+        listicon.append(weather['list'][i]['weather'][0]['icon'])
+        time = weather['list'][i]['dt_txt']
+        xtime = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+        formtime = xtime.strftime("%H:%M:%S")
+        listime.append(formtime)
+    print(time)
+    return {
+        'temperature':listemp,
+        'description':listdesc,
+        'icon':listicon,
+        'time':listime,
+        'main':listmain
+    }
+
 
 def get_loc():
     ip = get_ip()
-    print('getloc'+ip)
     city = ip.get('city')
     zipcode = ip['zip']
     url = 'http://www.datasciencetoolkit.org/maps/api/geocode/json?address={}+{}'.format(str(city),str(zipcode))
@@ -82,4 +117,6 @@ def error_404(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+'2019-08-29 12:00:00'
